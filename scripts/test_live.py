60f6ceb4e34650fdc4ee7f7fb81a2c7d73ebd545
zebra314 <kidalfonso@gmail.com>
2025-09-02 15:36:26 +0800

    Add test live

---
+++ scripts/test_live.py
@@ -0,0 +1,7 @@
+from camera_handler import CameraHandler
+from logging_config import setup_logging
+
+if __name__ == "__main__":
+    setup_logging()
+    camera_handler = CameraHandler()
+    camera_handler.live()
