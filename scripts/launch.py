23eafb11f135672e6819ce77755a2eab6b1f3a46
zabra314 <kidalfonso@gmail.com>
2025-10-13 17:33:36 +0800

    Replace RPi.GPIO with lgpio for GPIO handling in launch script

--- scripts/launch.py
+++ scripts/launch.py
@@ -1,4 +1,4 @@
-import RPi.GPIO as GPIO
+import lgpio
 import logging
 import time
 import threading
@@ -6,6 +6,9 @@ from src.setup_logging import setup_logging
 from src.camera import Camera
 
 RUN_DURATION = 20
+CHIP = 0 # usually gpiochip0
+INPUT_PIN = 3 # BCM 3, physical pin 5
+
 signal_detected = False
 task_thread = None
 camera = Camera()
@@ -13,11 +16,11 @@ camera = Camera()
 def my_task():
     logger.info("Task started.")
 
-    # while True:
-    #     logger.info("Task is running...")
-    #     time.sleep(1)
+    while True:
+        logger.info("Task is running...")
+        time.sleep(1)
 
-    camera.headless_record()
+    # camera.headless_record()
 
 def stop_task():
     global task_thread
@@ -45,13 +48,15 @@ if __name__ == "__main__":
     setup_logging()
     logger = logging.getLogger(__name__)
 
-    GPIO.setmode(GPIO.BCM)
+    # GPIO.setmode(GPIO.BCM)
+    # GPIO.setup(INPUT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    # GPIO.add_event_detect(INPUT_PIN, GPIO.RISING, callback=on_signal_detected, bouncetime=200)
 
-    # GPIO 3, PIN 5
-    INPUT_PIN = 3
-
-    GPIO.setup(INPUT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.add_event_detect(INPUT_PIN, GPIO.RISING, callback=on_signal_detected, bouncetime=200)
+    h = lgpio.gpiochip_open(CHIP)
+    lgpio.gpio_claim_input(h, INPUT_PIN)
+    lgpio.gpio_set_debounce_micros(CHIP, INPUT_PIN, 200000)
+    lgpio.gpio_claim_alert(h, INPUT_PIN, lgpio.RISING_EDGE)
+    cb = lgpio.callback(h, INPUT_PIN, lgpio.RISING_EDGE, on_signal_detected)
 
     try:
         while True:
@@ -71,4 +76,5 @@ if __name__ == "__main__":
         logger.info("Exiting program by user.")
 
     finally:
-        GPIO.cleanup()
+        cb.cancel()
+        lgpio.gpiochip_close(h)
